5340a3bbc8106d01220a19a6296a8f1a328280d1
Neil Mastroianni <suancho@gmail.com>
2019-08-08 09:27:30 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var data = {
+  title: "Psychotherapy"
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', data);
+});
+
+module.exports = router;

347a995fee27648225ec727a63a1a66b8760002f
Neil Mastroianni <suancho@gmail.com>
2019-08-09 08:03:25 -0400

    removing user route adding error to production

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var privacyRouter = require('./routes/privacy');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/privacy', privacyRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -34,6 +34,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
+  res.locals.status = err.status;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page

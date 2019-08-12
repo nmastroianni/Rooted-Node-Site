f6a1dbe90f901139de35f88ceb785272d3fe1d1f
Neil Mastroianni <suancho@gmail.com>
2019-08-12 08:11:58 -0400

    refactoring routes

--- routes/privacy/index.js
+++ routes/privacy/index.js
@@ -4,17 +4,20 @@ var data = {
   page: "Privacy Policy"
 }
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  if(req.baseUrl === "/privacy"){
-    res.render('privacy/index', data);
-  }
-  else if (req.baseUrl === "/privacy-policy.html" || "/privacy.html") {
-    res.redirect(301, "/privacy");
-  }
-  else {
-    next();
-  }
-});
 
-module.exports = router;
+
+module.exports = function() {
+  /* GET Privacy Policy page. */
+  router.get('/', function(req, res, next) {
+    if(req.baseUrl === "/privacy"){
+      res.render('privacy/index', data);
+    }
+    else if (req.baseUrl === "/privacy-policy.html" || "/privacy.html") {
+      res.redirect(301, "/privacy");
+    }
+    else {
+      next();
+    }
+  });
+  return router;
+} 

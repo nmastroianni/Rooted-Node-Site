fd7674ba416932c2ccc471c2657a9e558014220e
Neil Mastroianni <suancho@gmail.com>
2020-04-27 09:38:28 -0400

    making COVID alert link root relative

--- public/javascripts/alerts.js
+++ public/javascripts/alerts.js
@@ -1,5 +1,5 @@
 UIkit.notification({
-    message: '<span uk-icon="icon: warning" class="uk-margin-right"></span><a href="blog/telemedicine" class="uk-text-small">Please Visit Our Post on COVID-19 Services.</a>',
+    message: '<span uk-icon="icon: warning" class="uk-margin-right"></span><a href="/blog/telemedicine" class="uk-text-small">Please Visit Our Post on COVID-19 Services.</a>',
     status: 'danger',
     pos: 'top-right',
     timeout: 12000
